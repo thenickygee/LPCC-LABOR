35f6ca3b65f325c99091e2b988cd1aa3df7c9a32
nixxogee <nickgmitter@icloud.com>
2023-10-02 09:48:49 -0600

    coffee

--- src/App.js
+++ src/App.js
@@ -32,6 +32,18 @@ function App() {
           BBB Scam Alert: Don’t pay for free labor law posters
         </a>
       </header>
+      <script
+        data-name='BMC-Widget'
+        data-cfasync='false'
+        src='https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js'
+        data-id='ScammerBuster'
+        data-description='Support me on Buy me a coffee!'
+        data-message='I hope I saved you some money!'
+        data-color='#5F7FFF'
+        data-position='Right'
+        data-x_margin='18'
+        data-y_margin='18'
+      ></script>
     </div>
   );
 }
